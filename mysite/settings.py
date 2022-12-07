f23a769b9e4f5f730b64374b605cdf5576606ca2
Max Brüning <mail@maxbruening.de>
2022-12-07 13:44:24 +0100

    added db.json and script

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,11 +79,22 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+prod_config = {
     'default': dj_database_url.config(        # Feel free to alter this value to suit your needs.
     default='postgresql://postgres:postgres@localhost:5432/mysite',
-    conn_max_age=600    )}
+    conn_max_age=600    )
+}
 
+dev_config = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+database_config = prod_config if RENDER_EXTERNAL_HOSTNAME else dev_config
+
+DATABASES = database_config
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
